22f81874d66ef557f16a080d99c35fa0f577b388
hannahgov <hannah.sloat@gov.bc.ca>
2024-06-03 20:19:19 -0700

    removing blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,11 +47,12 @@ const config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
-          showReadingTime: true,
+          blogSidebarCount: 0
+          //showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          //editUrl:
+           // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: './src/css/custom.css',
